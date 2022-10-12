a990143b69028abbef809e217a62b945df0b26d1
shangwenyota <shangwenyota@localhost>
2022-10-12 15:27:56 -0500

    Studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,19 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		StdDraw.setPenColor(Color.RED);	
+		StdDraw.filledRectangle(0.5,0.5, .5, .25);
+		
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledCircle(0.5, 0.5, .3);
+		
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(0.5, 0.5, .15);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledCircle(0.5, 0.5, .07);
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,75 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		//colors are 3 ints
+		//shapes are 3 doubles
+		
+		
+//		//Ellipses
+		String shape = in.next();
+		
+		if (shape.equals("ellipse")) {
+			StdDraw.setPenColor(in.nextInt(), in.nextInt(), in.nextInt());
+			if (in.nextBoolean() == true) {
+				StdDraw.filledEllipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			} else {
+				StdDraw.ellipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+		}
+		
+		if (shape.equals("triangle")) {
+		StdDraw.setPenColor(in.nextInt(), in.nextInt(), in.nextInt());
+
+		if (in.nextBoolean() == false) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			StdDraw.line(x1, y1, x2, y2);
+			StdDraw.line(x2, y2, x3, y3);
+			StdDraw.line(x3, y3, x1, y1);
+			
+		} else {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			double [] x = {x1, x2, x3};
+			double [] y = {y1, y2, y3};
+			StdDraw.filledPolygon(x, y);
+		}
+	}
+		
+		
+		
+		//triangles
+//		if (in.next().equals("triangle")) {
+//			StdDraw.setPenColor(in.nextInt(), in.nextInt(), in.nextInt());
+//
+//			if (in.nextBoolean() == false) {
+//				double x1 = in.nextDouble();
+//				double y1 = in.nextDouble();
+//				double x2 = in.nextDouble();
+//				double y2 = in.nextDouble();
+//				double x3 = in.nextDouble();
+//				double y3 = in.nextDouble();
+//				StdDraw.line(x1, y1, x2, y2);
+//				StdDraw.line(x2, y2, x3, y3);
+//				StdDraw.line(x3, y3, x1, y1);
+//				
+//			}
+//		}
+		
+//		System.out.println(in.next());
+//		//Sets pen color
+//		StdDraw.setPenColor(in.nextInt(), in.nextInt(), in.nextInt());
+//		System.out.println(in.nextBoolean());
+//		//Draw Circle
+//		StdDraw.filledRectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+		
 	}
 }
